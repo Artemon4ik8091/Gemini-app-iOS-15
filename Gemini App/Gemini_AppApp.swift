9577103c2044e2433061be33d984437a1d7d5abf
aswer <yandex8091@gmail.com>
2026-06-10 11:03:23 +0300

    Initial Commit

---
+++ Gemini App/Gemini_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gemini_AppApp.swift
+//  Gemini App
+//
+//  Created by aswer on 10.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct Gemini_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
